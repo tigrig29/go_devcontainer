419dca38f4dfa806988930bf304e07cae3bb24ec
TigRig <tigrig.crex@gmail.com>
2022-10-20 05:06:09 +0000

    modified with reference to https://github.com/microsoft/vscode-remote-try-go

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 
-	"github.com/davecgh/go-spew/spew"
+	"github.com/tigrig29/vscode-remote-try-go/hello"
 )
 
+func handle(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, hello.Hello())
+}
+
 func main() {
-	fmt.Println("Hellow, Go!!")
-	debug_str := "Hellow, Module!!"
-	spew.Dump(debug_str)
+	portNumber := "9000"
+	http.HandleFunc("/", handle)
+	fmt.Println("Server listening on port ", portNumber)
+	http.ListenAndServe(":"+portNumber, nil)
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tigrig29/vscode-remote-try-go
+
+go 1.15
